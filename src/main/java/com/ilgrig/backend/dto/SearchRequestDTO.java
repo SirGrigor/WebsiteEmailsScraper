00796dfb92418825a70e5c1b085dfb873750a440
gabriel <grigorjev.ilja@gmail.com>
2021-01-28 16:48:51 +0200

    GEt emails

--- src/main/java/com/ilgrig/backend/dto/SearchRequestDTO.java
+++ src/main/java/com/ilgrig/backend/dto/SearchRequestDTO.java
@@ -8,6 +8,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SearchRequestDTO {
-    private String hostName;
     private String url;
 }
--- src/main/java/com/ilgrig/backend/service/QueryService.java
+++ src/main/java/com/ilgrig/backend/service/QueryService.java
@@ -1,46 +1,62 @@
 package com.ilgrig.backend.service;
 
+import com.ilgrig.backend.dto.QueryDTO;
+import com.ilgrig.backend.repository.ProspectRepository;
+import org.apache.commons.net.whois.WhoisClient;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Service;
+
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import com.ilgrig.backend.entity.Prospect;
-import com.ilgrig.backend.dto.QueryDTO;
-import com.ilgrig.backend.repository.ProspectRepository;
-import org.jsoup.nodes.Document;
-import org.jsoup.Jsoup;
-import org.springframework.stereotype.Service;
-
 @Service
 public class QueryService {
     private final ProspectRepository prospectRepository;
+    private static Pattern pattern;
+    private Matcher matcher;
+
+    private static final String WHOIS_SERVER_PATTERN = "Whois Server:\\s(.*)";
+
+    static {
+        pattern = Pattern.compile(WHOIS_SERVER_PATTERN);
+    }
 
     public QueryService(ProspectRepository prospectRepository) {
         this.prospectRepository = prospectRepository;
     }
 
     public void getSingularQuery(QueryDTO queryDTO) throws IOException {
-        queryDTO.getUrls().forEach(url -> {
-            Document doc = null;
-            try {
-                doc = Jsoup.connect(url.getUrl()).get();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        WhoisClient whois = new WhoisClient();
+            whois.connect("whois.iana.org", 43);
+            System.out.println(whois.query(queryDTO.getUrls().get(0).getUrl()));
+            whois.disconnect();
+
+        System.out.println(getEmailsByUrl("https://" + queryDTO.getUrls().get(0).getUrl()));
+    }
+
+    public Set<String> getEmailsByUrl(String url) {
+        Document doc;
+        Set<String> emailSet = new HashSet<>();
+
+        try {
+            doc = Jsoup.connect(url)
+                    .userAgent("Mozilla")
+                    .get();
 
             Pattern p = Pattern.compile("[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+");
-            Matcher matcher = p.matcher(doc.text());
-            Set<String> emails = new HashSet<String>();
+            Matcher matcher = p.matcher(doc.body().html());
             while (matcher.find()) {
-                emails.add(matcher.group());
-                Prospect prospect = new Prospect();
-                prospect.setProspectEmails(matcher.group());
-                prospectRepository.save(prospect);
+                emailSet.add(matcher.group());
             }
-        });
-
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
+        return emailSet;
     }
+}
+
